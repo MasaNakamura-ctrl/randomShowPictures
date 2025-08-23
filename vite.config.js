c1ea93f0d79697d41d836d811595636d2775ac23
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-08-23 10:51:52 +0900

    Update:vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/randomShowPictures/',  // アプリをプッシュしたリポジトリ名
+  base: '/randomShowPictures/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     host: "0.0.0.0",
     port: 3000,
